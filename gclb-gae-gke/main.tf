d656e4abb8d397cac6a235ab544270f8752dd71e
Greg Bray <Greg.Bray@gmail.com>
2022-04-04 20:43:30 -0600

    switch to gcs for tfstate

--- gclb-gae-gke/main.tf
+++ gclb-gae-gke/main.tf
@@ -1,6 +1,14 @@
+# https://www.terraform.io/language/settings/backends/gcs
+terraform {
+  backend "gcs" {
+    bucket  = "tf-state-clusterseiwzln"
+    prefix  = "terraform/test/state"
+  }
+}
+
 provider "google" {
   project     = "demo2021-310119"
   region      = "us-central1"
   zone        = "us-central1-c"
+  
 }
-
