fa1d264f5c6f090c805099398d70cc4f68751bac
Greg Bray <greg.bray@gmail.com>
2025-05-05 13:06:59 -0600

    Update zzz_echoserver_WIP.yaml and gcs-buckets.py

--- kind-identity-federation/gcs-buckets.py
+++ kind-identity-federation/gcs-buckets.py
@@ -1,11 +1,10 @@
 import os
 from google.cloud import storage
 
-os.environ['GOOGLE_CLOUD_PROJECT'] = "gregbray-repo"
+os.environ['GOOGLE_CLOUD_PROJECT'] = "gregbray-vpc"
 
 # Instantiates a client
 storage_client = storage.Client()
 
 # List all the buckets available
-for bucket in storage_client.list_buckets():
-    print(bucket)
+[print(bucket) for bucket in storage_client.list_buckets()]
